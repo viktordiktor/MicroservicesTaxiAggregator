4877feb1632716de8244b44cecd82f7c8d948a36
viktordiktor <eversum69@gmail.com>
2024-01-31 13:43:55 +0300

    fix: refactoring

--- passenger-service/src/main/java/com/nikonenko/passengerservice/services/PassengerServiceImpl.java
+++ passenger-service/src/main/java/com/nikonenko/passengerservice/services/PassengerServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
