db420e29bbfe50384401f24f7ecd61bf9f7ade2f
viktordiktor <eversum69@gmail.com>
2024-02-08 21:06:26 +0300

    init: discovery-server

--- ride-service/src/main/java/com/nikonenko/rideservice/RideServiceApplication.java
+++ ride-service/src/main/java/com/nikonenko/rideservice/RideServiceApplication.java
@@ -3,9 +3,11 @@ package com.nikonenko.rideservice;
 import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableMongock
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RideServiceApplication {
 
     public static void main(String[] args) {
