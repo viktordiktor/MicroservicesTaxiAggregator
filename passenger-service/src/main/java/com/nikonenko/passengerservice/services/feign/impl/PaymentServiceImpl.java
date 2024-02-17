2f2d3adfc112f0a66480ab48c3edff8e656cd210
viktordiktor <eversum69@gmail.com>
2024-02-17 11:27:46 +0300

    init: circuit breaker in passenger service

--- passenger-service/src/main/java/com/nikonenko/passengerservice/services/feign/impl/PaymentServiceImpl.java
+++ passenger-service/src/main/java/com/nikonenko/passengerservice/services/feign/impl/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nikonenko.passengerservice.services.feign.impl;
 
+import com.nikonenko.passengerservice.dto.PageResponse;
 import com.nikonenko.passengerservice.dto.feign.payment.CustomerCalculateRideRequest;
 import com.nikonenko.passengerservice.dto.feign.payment.CustomerCalculateRideResponse;
 import com.nikonenko.passengerservice.dto.feign.payment.CustomerChargeRequest;
@@ -9,31 +10,51 @@ import com.nikonenko.passengerservice.dto.feign.payment.CustomerCreationResponse
 import com.nikonenko.passengerservice.dto.feign.payment.CustomerExistsResponse;
 import com.nikonenko.passengerservice.feign.PaymentFeignClient;
 import com.nikonenko.passengerservice.services.feign.PaymentService;
+import com.nikonenko.passengerservice.utils.ExceptionList;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentFeignClient paymentFeignClient;
 
     @Override
+    @CircuitBreaker(name = "paymentBreaker", fallbackMethod = "fallbackPaymentService")
     public CustomerChargeResponse createCharge(CustomerChargeRequest customerChargeRequest) {
         return paymentFeignClient.customerCharge(customerChargeRequest);
     }
 
     @Override
+    @CircuitBreaker(name = "paymentBreaker", fallbackMethod = "fallbackPaymentService")
     public CustomerExistsResponse checkCustomerExists(Long passengerId) {
         return paymentFeignClient.isCustomerExists(passengerId);
     }
 
     @Override
+    @CircuitBreaker(name = "paymentBreaker", fallbackMethod = "fallbackPaymentService")
     public CustomerCalculateRideResponse calculateRidePrice(CustomerCalculateRideRequest calculateRideRequest) {
         return paymentFeignClient.calculateRidePrice(calculateRideRequest);
     }
 
     @Override
+    @CircuitBreaker(name = "paymentBreaker", fallbackMethod = "fallbackPaymentService")
     public CustomerCreationResponse createCustomer(CustomerCreationRequest customerCreationRequest) {
         return paymentFeignClient.createCustomer(customerCreationRequest);
+    }
+
+    public PageResponse<CustomerChargeResponse> fallbackPaymentService(Exception ex) {
+        log.info("Exception during request to Payment Service: {}", ex.getMessage());
+        return PageResponse.<CustomerChargeResponse>builder()
+                .objectList(Collections.emptyList())
+                .totalElements(0)
+                .totalPages(0)
+                .errorMessage(ExceptionList.PAYMENT_SERVICE_NOT_AVAILABLE.getValue())
+                .build();
     }
 }
--- passenger-service/src/main/java/com/nikonenko/passengerservice/services/feign/impl/RideServiceImpl.java
+++ passenger-service/src/main/java/com/nikonenko/passengerservice/services/feign/impl/RideServiceImpl.java
@@ -8,31 +8,51 @@ import com.nikonenko.passengerservice.dto.feign.ride.CreateRideRequest;
 import com.nikonenko.passengerservice.dto.feign.ride.RideResponse;
 import com.nikonenko.passengerservice.feign.RideFeignClient;
 import com.nikonenko.passengerservice.services.feign.RideService;
+import com.nikonenko.passengerservice.utils.ExceptionList;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RideServiceImpl implements RideService {
     private final RideFeignClient rideFeignClient;
 
     @Override
+    @CircuitBreaker(name = "rideBreaker", fallbackMethod = "fallbackRideService")
     public CalculateDistanceResponse getRideDistance(CalculateDistanceRequest customerChargeRequest) {
         return rideFeignClient.calculateDistance(customerChargeRequest);
     }
 
     @Override
+    @CircuitBreaker(name = "rideBreaker", fallbackMethod = "fallbackRideService")
     public RideResponse createRide(CreateRideRequest createRideRequest) {
         return rideFeignClient.createRideRequest(createRideRequest);
     }
 
     @Override
+    @CircuitBreaker(name = "rideBreaker", fallbackMethod = "fallbackRideService")
     public CloseRideResponse closeRide(String rideId) {
         return rideFeignClient.closeRide(rideId);
     }
 
     @Override
+    @CircuitBreaker(name = "rideBreaker", fallbackMethod = "fallbackRideService")
     public PageResponse<RideResponse> getRidesByPassengerId(Long passengerId) {
         return rideFeignClient.getRidesByPassengerId(passengerId);
+    }
+
+    public PageResponse<RideResponse> fallbackRideService(Exception ex) {
+        log.info("Exception during request to Ride Service: {}", ex.getMessage());
+        return PageResponse.<RideResponse>builder()
+                .objectList(Collections.emptyList())
+                .totalElements(0)
+                .totalPages(0)
+                .errorMessage(ExceptionList.RIDE_SERVICE_NOT_AVAILABLE.getValue())
+                .build();
     }
 }
