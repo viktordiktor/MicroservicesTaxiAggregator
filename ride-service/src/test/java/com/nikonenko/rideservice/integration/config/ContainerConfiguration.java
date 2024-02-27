18be8387f0c0d6fcce5b844c6fa8826ec66ab808
Viktor Nikonenko <117480569+viktordiktor@users.noreply.github.com>
2024-02-27 17:31:40 +0300

    init: Ride service Integration Tests

---
+++ ride-service/src/test/java/com/nikonenko/rideservice/integration/config/ContainerConfiguration.java
@@ -0,0 +1,32 @@
+package com.nikonenko.rideservice.integration.config;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContainerConfiguration {
+
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse(
+            "mongo:latest"
+    ));
+
+    @BeforeAll
+    static void beforeAll() {
+        mongoDBContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mongoDBContainer.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}
