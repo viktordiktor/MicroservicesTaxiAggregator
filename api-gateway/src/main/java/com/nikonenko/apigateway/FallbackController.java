d584431ac63b136e33602ae4d239a587f12fc8ae
Viktor Nikonenko <117480569+viktordiktor@users.noreply.github.com>
2024-03-06 13:13:32 +0300

    fix: circuit breaker in api-gateway

--- api-gateway/src/main/java/com/nikonenko/apigateway/FallbackController.java
+++ api-gateway/src/main/java/com/nikonenko/apigateway/FallbackController.java
@@ -1,27 +1,28 @@
 package com.nikonenko.apigateway;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/fallback")
 public class FallbackController {
-
-    @RequestMapping("/fallback/passenger-service")
+    @GetMapping("/passenger-service")
     public String passengerServiceFallback() {
         return "Passenger service is down right now. Please try again later.";
     }
 
-    @RequestMapping("/fallback/driver-service")
+    @GetMapping("/driver-service")
     public String driverServiceFallback() {
         return "Driver service is down right now. Please try again later.";
     }
 
-    @RequestMapping("/fallback/ride-service")
+    @GetMapping("/ride-service")
     public String rideServiceFallback() {
         return "Rides service is down right now. Please try again later.";
     }
 
-    @RequestMapping("/fallback/payment-service")
+    @GetMapping("/payment-service")
     public String paymentServiceFallback() {
         return "Payment service is down right now. Please try again later.";
     }
