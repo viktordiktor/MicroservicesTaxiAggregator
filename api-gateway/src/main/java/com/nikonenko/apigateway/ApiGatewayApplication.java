589bac47a1ab3e931e6a9bc3aa711dae6acde96f
Viktor Nikonenko <117480569+viktordiktor@users.noreply.github.com>
2024-03-11 12:53:03 +0300

    init: elk + zipkin

--- api-gateway/src/main/java/com/nikonenko/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/nikonenko/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,15 @@
 package com.nikonenko.apigateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +19,25 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory
+            (final CircuitBreakerRegistry circuitBreakerRegistry, final TimeLimiterRegistry timeLimiterRegistry) {
+        ReactiveResilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory =
+                new ReactiveResilience4JCircuitBreakerFactory(circuitBreakerRegistry, timeLimiterRegistry);
+        reactiveResilience4JCircuitBreakerFactory.configureCircuitBreakerRegistry(circuitBreakerRegistry);
+        reactiveResilience4JCircuitBreakerFactory.configureDefault(id -> {
+            CircuitBreakerConfig circuitBreakerConfig = circuitBreakerRegistry.find(id).isPresent()
+                    ? circuitBreakerRegistry.find(id).get().getCircuitBreakerConfig()
+                    : circuitBreakerRegistry.getDefaultConfig();
+            TimeLimiterConfig timeLimiterConfig = timeLimiterRegistry.find(id).isPresent()
+                    ? timeLimiterRegistry.find(id).get().getTimeLimiterConfig()
+                    : timeLimiterRegistry.getDefaultConfig();
+
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(circuitBreakerConfig)
+                    .timeLimiterConfig(timeLimiterConfig)
+                    .build();
+        });
+        return reactiveResilience4JCircuitBreakerFactory;
+    }
 }
