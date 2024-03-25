85d13d2b59b29f06f475d62404cd1c4bbe0b3c88
Viktor Nikonenko <117480569+viktordiktor@users.noreply.github.com>
2024-03-25 12:12:47 +0300

    fix: fixed fallback controller

--- api-gateway/src/main/java/com/nikonenko/apigateway/controllers/FallbackController.java
+++ api-gateway/src/main/java/com/nikonenko/apigateway/controllers/FallbackController.java
@@ -1,29 +1,39 @@
 package com.nikonenko.apigateway.controllers;
 
 import com.nikonenko.apigateway.utils.ExceptionList;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
-    @GetMapping("/passenger-service")
+    @RequestMapping(value = "/passenger-service",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
+    @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
     public String passengerServiceFallback() {
         return ExceptionList.PASSENGER_SERVICE_DOWN.getValue();
     }
 
-    @GetMapping("/driver-service")
+    @RequestMapping(value = "/driver-service",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
+    @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
     public String driverServiceFallback() {
         return ExceptionList.DRIVER_SERVICE_DOWN.getValue();
     }
 
-    @GetMapping("/ride-service")
+    @RequestMapping(value = "/ride-service",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
+    @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
     public String rideServiceFallback() {
         return ExceptionList.RIDE_SERVICE_DOWN.getValue();
     }
 
-    @GetMapping("/payment-service")
+    @RequestMapping(value = "/payment-service",
+            method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE})
+    @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
     public String paymentServiceFallback() {
         return ExceptionList.PAYMENT_SERVICE_DOWN.getValue();
     }
